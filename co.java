9d194897749fe4d690833abc2c4ab96e3a87b928
Roy-adi <107671147+Roy-adi@users.noreply.github.com>
2022-06-22 13:46:29 +0530

    Add files via upload

---
+++ co.java
@@ -0,0 +1,16 @@
+class co{
+    public static void main(String[]args){
+
+     int a= 2;
+     int b= 4;
+     int sub= a-b;
+     int multi = a*b;
+     int div  = a/b;
+     int add = a+b;
+     System.out.println(sub);
+     System.out.println(multi);
+     System.out.println(div);
+     System.out.println(add);
+}
+
+}
