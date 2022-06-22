20845687b9e9f51c4ba0d09d4864a417da03d054
Roy-adi <107671147+Roy-adi@users.noreply.github.com>
2022-06-22 20:14:13 +0530

    Add files via upload

---
+++ CJAVA.java
@@ -0,0 +1,9 @@
+class CJAVA{
+    public static void main(String[]args){
+     System.out.println(" course : java " );
+     System.out.println(" Stream : CE  ");
+                           
+
+}
+
+} 
