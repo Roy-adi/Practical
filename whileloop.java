d760c2510d6332e7dd7d57f964748c85b1758410
Aditya Roy <107671147+Roy-adi@users.noreply.github.com>
2022-06-27 13:10:01 +0530

    Create whileloop.java

---
+++ whileloop.java
@@ -0,0 +1,11 @@
+public class Whileloop {
+    public static void main(String[]args){
+        int i= 0;
+        System.out.println("First 10 even no \n");
+        while (i<=10){
+        System.out.println(i);
+        i= i+1;    
+        }
+    }
+    
+}
