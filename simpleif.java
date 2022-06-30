8c7f1312f2fbf488a2736f2b117389ebdd6510b3
Aditya Roy <107671147+Roy-adi@users.noreply.github.com>
2022-06-30 11:04:01 +0530

    Create simpleif.java

---
+++ simpleif.java
@@ -0,0 +1,10 @@
+public class simpleif {
+    public static void main(String[] args) {
+      int x = 10;
+      int y = 18;
+      if (x > y) {
+        System.out.println("x is greater than y");
+      }  
+    }
+  }
+  
