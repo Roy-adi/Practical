7d5184aa6c9c1308d2c703b741822c1e68ec2341
Aditya Roy <107671147+Roy-adi@users.noreply.github.com>
2022-06-30 11:08:52 +0530

    Create nastedif.java

---
+++ nastedif.java
@@ -0,0 +1,20 @@
+class nastedif{
+
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=20;
+		
+		if(a==10){
+
+			if(b!=20){
+				System.out.println("yes");
+			}
+			
+			else{
+				System.out.println("no");
+			}
+		}
+    }
+}
+
