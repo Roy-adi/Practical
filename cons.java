b927de031ca4a80a5e061baadd3215a83fb66a7c
Aditya Roy <107671147+Roy-adi@users.noreply.github.com>
2022-06-30 12:22:43 +0530

    Create cons.java

---
+++ cons.java
@@ -0,0 +1,14 @@
+public class cons {
+    String name ;
+    cons(){
+        name = "Aditya";
+        System.out.println(" name");
+    
+    }
+
+
+public static void main (String[]args){
+    cons obj = new cons();
+    System.out.println(" the name is " + obj.name);
+}
+}
