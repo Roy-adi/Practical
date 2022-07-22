3ba5093d405b4f43a5a77001ddbf2c530febe59d
Aditya Roy <107671147+Roy-adi@users.noreply.github.com>
2022-07-22 10:37:47 +0530

    Create odd_place.java

---
+++ odd_place.java
@@ -0,0 +1,9 @@
+public class array4 {
+    public static void main(String[]args){
+
+        String [] s = {"a","b","c","d","e","f"};
+        for( int i= 0; i < s.length; i=i+2){
+           System.out.println(s[i]);
+        }
+    }
+}
